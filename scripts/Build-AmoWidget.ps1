d1c745b8a23b37a2c2abc6962d4f1eec2ea786fa
CheyMay <cheymay@yandex.ru>
2026-08-16 18:12:35 +0300

    Build amoCRM widget zip with POSIX paths

--- scripts/Build-AmoWidget.ps1
+++ scripts/Build-AmoWidget.ps1
@@ -7,7 +7,47 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
+Add-Type -AssemblyName System.IO.Compression
 Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+function New-WidgetArchive {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$SourceDirectory,
+        [Parameter(Mandatory = $true)]
+        [string]$DestinationPath
+    )
+
+    $stream = [IO.File]::Open($DestinationPath, [IO.FileMode]::CreateNew)
+    $archive = $null
+    try {
+        $archive = [IO.Compression.ZipArchive]::new(
+            $stream,
+            [IO.Compression.ZipArchiveMode]::Create,
+            $false
+        )
+        $files = Get-ChildItem -LiteralPath $SourceDirectory -Recurse -File | Sort-Object FullName
+        foreach ($file in $files) {
+            $relativePath = $file.FullName.Substring($SourceDirectory.Length).TrimStart([char[]]@('\', '/'))
+            $entryName = $relativePath.Replace('\', '/')
+            $entry = $archive.CreateEntry($entryName, [IO.Compression.CompressionLevel]::Optimal)
+            $entryStream = $entry.Open()
+            $inputStream = $null
+            try {
+                $inputStream = [IO.File]::OpenRead($file.FullName)
+                $inputStream.CopyTo($entryStream)
+            }
+            finally {
+                if ($inputStream) { $inputStream.Dispose() }
+                $entryStream.Dispose()
+            }
+        }
+    }
+    finally {
+        if ($archive) { $archive.Dispose() }
+        $stream.Dispose()
+    }
+}
 
 $widgetRoot = [IO.Path]::GetFullPath($WidgetDirectory)
 $source = [IO.Path]::GetFullPath((Join-Path $widgetRoot 'front'))
@@ -33,12 +73,7 @@ foreach ($path in @($versioned, $latest)) {
     }
 }
 
-[IO.Compression.ZipFile]::CreateFromDirectory(
-    $source,
-    $versioned,
-    [IO.Compression.CompressionLevel]::Optimal,
-    $false
-)
+New-WidgetArchive -SourceDirectory $source -DestinationPath $versioned
 Copy-Item -LiteralPath $versioned -Destination $latest
 
 Get-Item -LiteralPath $versioned, $latest | Select-Object FullName, Length
